7e4d0d0740848c81a126554dd31e22d468237a60
coldhands1591 <suphatphlaenglit@gmail.com>
2023-02-01 21:18:42 +0700

    test update

--- me_mee/test.py
+++ me_mee/test.py
@@ -1,1 +1,23 @@
 print('hello mao-python')
+
+#==== basic =======
+#Syntax
+#Valiables
+#Data Types
+    #String
+    #Number
+    #Booleans
+    #Arrays
+        #List
+        #Tuples
+    #Sets
+    #Dictionaries
+#Conditions (#Booleans)
+#Operators
+#Loops
+
+#=== medium ====
+#Functions
+#Class
+#=== Advance ====
+#OOP (concept)
