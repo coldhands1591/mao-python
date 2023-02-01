1bc2f973306ccb59dc1a4498ea34fdfeff706dba
coldhands1591 <suphatphlaenglit@gmail.com>
2023-02-01 22:58:51 +0700

    day 1

---
+++ master/data_type/string.py
@@ -0,0 +1,22 @@
+a = 'Hello'
+strMultiLine = """
+oldhands@Suphats-MacBook-Air data_type
+oldhands@Suphats-MacBook-Air data_type
+oldhands@Suphats-MacBook-Air data_type
+"""
+strArray = "Hello" #[H e l l o] array
+                    #0 1 2 3 4 => index array
+print('get length: ' + str(len(strArray)))
+# print(strArray[0]) #=> get data by index
+for item in strArray: #=> get data by for loop
+    if item == 'o':# true or false
+        getO = item
+        # print(getO)
+    # print(item)
+
+# fluits  = ['Orange','Banana','Cherry'] #=> data list
+# for item in fluits:
+#     for item2 in item:
+#         print(item2)
+#     print(item)
+
