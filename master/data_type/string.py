6e565d147778fe1b9539ba13e6e669888a3b9165
coldhands1591 <suphatphlaenglit@gmail.com>
2023-02-16 20:50:54 +0700

    string

--- master/data_type/string.py
+++ master/data_type/string.py
@@ -6,12 +6,18 @@ oldhands@Suphats-MacBook-Air data_type
 """
 strArray = "Hello" #[H e l l o] array
                     #0 1 2 3 4 => index array
-print('get length: ' + str(len(strArray)))
+# if "c" not in strArray:
+#     print("No 'c' is not present" )
+# if "e" in strArray:
+#     print("Yes, 'e' is present.")
+
+#print('get length: ' + str(len(strArray)))
+# print(strArray[1])
 # print(strArray[0]) #=> get data by index
-for item in strArray: #=> get data by for loop
-    if item == 'o':# true or false
-        getO = item
-        # print(getO)
+# for item in strArray: #=> get data by for loop
+#     if item == 'o':# true or false
+#         getO = item
+#         print(getO)
     # print(item)
 
 # fluits  = ['Orange','Banana','Cherry'] #=> data list
@@ -19,4 +25,49 @@ for item in strArray: #=> get data by for loop
 #     for item2 in item:
 #         print(item2)
 #     print(item)
+
+
+#=== Slice String ===
+# b = "Hello,World!"
+#     #01234567891011 =>index string
+# print(b[2:5])#=> b[start index:end index]
+# print(b[:5])
+# print(b[7:])
+
+#=== Modify String ====
+firstName = ' SupHat'
+lastName  = 'PHLaengliT '
+fullName = firstName+'_'+lastName
+# print(firstName.upper())
+# print(lastName.lower())
+# print(fullName.strip())
+newFullName = fullName.strip() #SupHit_PHLiengliT
+# print(newFullName.replace('a','i'))
+# print(newFullName.split('_')) #=> ['SupHat', 'PHLaengliT']
+arrFullName = newFullName.split('_') #=> ['SupHat', 'PHLaengliT']
+newFirstName = arrFullName[0]
+newLastName = arrFullName[1]
+
+countEFirstName = 0
+for item in newFirstName:
+    if item == 'e':
+        countEFirstName = countEFirstName+1
+
+countELastName = 0
+for item in newLastName:
+    if item == 'e':
+        countELastName = countELastName+1
+
+if countEFirstName > countELastName:
+    countResult = countEFirstName - countELastName
+    print('FistName more than LastName: '+str(countResult))
+elif countEFirstName == countELastName:
+    print('FistName equa to LastName: '+str(countELastName))
+elif countEFirstName < countELastName:
+    countResult  = countELastName - countEFirstName
+    print('FistName less than LastName: '+str(countResult))
+
+# print(newLastName)
+
+
 
--- master/day1.py
+++ master/day1.py
@@ -1,1 +1,22 @@
-print('test')
+# int number = 10 static type (C#)
+#=== Valiables (a-z,A-Z,_,0-9) =====
+number  = 10 # value  = 10 ,data type int (dynamic type)
+# number = '10'
+# number = True
+# print(type(number)) => cmd+k cmd+c (comment),cmd+k cmd+u(un comment)
+x,y,z = 'Orange','Banana','Cherry'
+fluits  = ['Orange','Banana','Cherry']
+a,b,c = fluits
+firstName = 'first Name'
+FirstName = 'new First Name'
+LastName = 'Last Name'
+# print(a+' '+b+' '+c)
+
+    #=== Global Valiable ===
+# myName = 'test name 1' #global var
+
+def getMyName(): #Funciton
+    myName = 'sara' #local var
+    print(myName)
+# getMyName() # call func
+
